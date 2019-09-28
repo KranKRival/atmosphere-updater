f53d426c91d4724bafca93fe5f201fb8c1614f3f
KranK <krankshuffle@gmail.com>
2019-09-28 12:22:06 +0300

    Added full touch support
    Added full touch support.
    
    Added confirm box pop-up when selecting an option.

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 #include <switch.h>
 
 #include "util.h"
+#include "touch.h"
 #include "menu.h"
 #include "unzip.h"
 #include "download.h"
@@ -64,20 +65,20 @@ int main(int argc, char **argv)
 
     // set the cursor position to 0.
     short cursor = 0;
-    u32 prev_touchcount = 0;
 
-    // TODO: touch
-    /*u32 tch = 0;
+    // touch variables.
+    int touch_lock = OFF;
+    u32 tch = 0;
     touchPosition touch;
-    hidTouchRead(&touch, tch);
-    if (KEY_TOUCH && touch.px > 530 && touch.px < 1200 && touch.py > 130-20 && touch.py < 130-20+70)*/
     
     // muh loooooop
     while(appletMainLoop())
     {
-        
+        // scan for button / touch input each frame.
         hidScanInput();
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+        hidTouchRead(&touch, tch);
+        u32 touch_count = hidTouchCount();
 
         // main menu display
         printOptionList(cursor);
@@ -97,114 +98,49 @@ int main(int argc, char **argv)
         }
 
         // select option
-        if (kDown & KEY_A)
+        if (kDown & KEY_A || (touch_lock == OFF && touch.px > 530 && touch.px < 1200 && touch.py > 130 && touch.py < 640))
         {
+            // check if the user used touch to enter this option.
+            if (touch_lock == OFF && touch_count > 0)
+                cursor = touch_cursor(touch.px, touch.py);
+
             switch (cursor)
             {
             case UP_AMS:
-                update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
+                if (yesNoBox(cursor, 390, 250, "Update Atmosphere?") == YES)
+                    update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
                 break;
 
             case UP_AMS_NOINI:
-                update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
+                if (yesNoBox(cursor, 390, 250, "Update Atmosphere\n(ignoring .ini files)?") == YES)
+                    update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
                 break;
 
             case UP_HEKATE:
-                update_ams_hekate(HEKATE_URL, HEKATE_OUTPUT, cursor);
+                if (yesNoBox(cursor, 390, 250, "Update Hekate?") == YES)
+                    update_ams_hekate(HEKATE_URL, HEKATE_OUTPUT, cursor);
                 break;
 
             case UP_APP:
-                update_app();
+                if (yesNoBox(cursor, 390, 250, "Update App?") == YES)
+                    update_app();
                 break;
 
             case REBOOT_PAYLOAD:
-                reboot_payload("/atmosphere/reboot_payload.bin");
+                if (yesNoBox(cursor, 390, 250, "Reboot to Payload?") == YES)
+                    reboot_payload("/atmosphere/reboot_payload.bin");
                 break;
             }
         }
-        
-        // exit
-        if (kDown & KEY_PLUS) break;
 
-        // oh boi o love touch so much, KranK touched this. loops are also cool :P
-        // read touch inputs / touch goes before hid inputs ...
-        
-        touchPosition touch;
-        u32 touch_count = hidTouchCount();
+        // exit...
+        if (kDown & KEY_PLUS || (touch.px > 1145 && touch.px < 1280 && touch.py > 675 && touch.py < 720))
+            break;
 
-        if (touch_count != prev_touchcount)
-        {
-            prev_touchcount = touch_count;
+        // lock touch if the user has already touched the screen (touch tap).
+        if (touch_count > 0) touch_lock = ON;
+        else touch_lock = OFF;
 
-        }
-
-        for(u32 i = 0; i < touch_count; i++)
-        {
-            //Read the touch screen coordinates
-            hidTouchRead(&touch, i);
-            
-        }
-
-        // A key press by touch
-        if (touch.px > 1010 && touch.py > 675  && touch.px < 1100)
-        {
-            switch (cursor)
-            {
-            case UP_AMS:
-                update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
-                break;
-
-            case UP_AMS_NOINI:
-                update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
-                break;
-
-            case UP_HEKATE:
-                update_ams_hekate(HEKATE_URL, HEKATE_OUTPUT, cursor);
-                break;
-
-            case UP_APP:
-                update_app();
-                break;
-
-            case REBOOT_PAYLOAD:
-                reboot_payload("/atmosphere/reboot_payload.bin");
-                break;
-            }
-           
-        }
-
-        // actual option press by touch
-        else if (touch.px > 500 && touch.py > 130 && touch.py < 180 && touch.px < 1200)
-        {
-            cursor = UP_AMS;
-            update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
-        }
-        else if (touch.px > 500 && touch.py > 230 && touch.py < 280 && touch.px < 1200)
-        {
-            cursor = UP_AMS_NOINI;
-            update_ams_hekate(AMS_URL, AMS_OUTPUT, cursor);
-        }
-        else if (touch.px > 500 && touch.py > 330 && touch.py < 380 && touch.px < 1200)
-        {
-            cursor = UP_HEKATE;
-            update_ams_hekate(HEKATE_URL, HEKATE_OUTPUT, cursor);
-        }
-        else if (touch.px > 500 && touch.py > 430 && touch.py < 480 && touch.px < 1200)
-        {
-            cursor = UP_APP;
-            update_app();
-        }
-        else if (touch.px > 500 && touch.py > 530 && touch.py < 580 && touch.px < 1200)
-        {
-            cursor = REBOOT_PAYLOAD;
-            reboot_payload("/atmosphere/reboot_payload.bin");
-        }
-        
-        // exit by touch 
-        else if (touch.px > 1200 && touch.py > 675  && touch.px < 1240) break;
-
-        // reset last touch
-        touch.px = 0;
         // display render buffer
         updateRenderer();
     }
